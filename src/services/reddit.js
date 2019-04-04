4cee94e60465a4a7dea2c54d6f55ecc4d1fca087
Rafael Fragoso <rafaelfragosom@gmail.com>
2019-04-04 20:41:41 -0300

    Adding the reddit service with endpoint to get the top 50 and get the details

---
+++ src/services/reddit.js
@@ -0,0 +1,25 @@
+import fetch from 'node-fetch'
+
+export default class Reddit {
+  constructor(options = {limit: 50}) {
+    this.options = options
+  }
+
+  get(nextPage) {
+    let url = `https://www.reddit.com/r/all/top.json?limit=${this.options.limit}`
+
+    if (nextPage !== null) {
+      url += `&after=${nextPage}&count=${this.options.limit}`
+    }
+
+    return fetch(url)
+            .then(response => response.json())
+            .then(res => res.data.children)
+  }
+
+  getPost(id, type) {
+    return fetch(`https://www.reddit.com/api/info.json?id=${type}_${id}`)
+            .then(response => response.json())
+            .then(res => res.data.children)
+  }
+}
