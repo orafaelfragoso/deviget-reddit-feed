e11e53b8f32ea51b9bf8e263377d37f4a223528d
Rafael Fragoso <rafaelfragosom@gmail.com>
2019-04-07 22:42:07 -0300

    Styling the DetailContainer

--- src/containers/DetailContainer.js
+++ src/containers/DetailContainer.js
@@ -3,18 +3,21 @@ import { connect } from 'react-redux'
 import { openDrawer } from '../actions/drawer.actions'
 import styles from './stylesheets/DetailContainer.module.scss';
 import Header from '../components/Header'
+import Paper from '@material-ui/core/Paper'
 
 class DetailContainer extends Component {
   render() {
+    const { mobile, openDrawer, selectedPost } = this.props
+
     return (
       <div className={styles.Container}>
-        <Header title="Details" showMenu={this.props.mobile} onMenuClick={this.props.openDrawer} />
-        {this.props.selectedPost && (
-          <React.Fragment>
-            <p><img src={this.props.selectedPost.image} /></p>
-            <p>{this.props.selectedPost.title}</p>
-            <p>{this.props.selectedPost.description}</p>
-          </React.Fragment>
+        <Header title="Details" showMenu={mobile} onMenuClick={openDrawer} />
+        {selectedPost && (
+          <Paper className={styles.Content}>
+            <p><img src={selectedPost.image} alt={selectedPost.description} className={styles.Image} /></p>
+            <p className={styles.Title}>{selectedPost.title}</p>
+            <p className={styles.Description}>{selectedPost.description}</p>
+          </Paper>
         )}
       </div>
     );
